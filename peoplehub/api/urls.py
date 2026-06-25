8a2985c26b566e1d9e89e16abbcaab81250e5840
AlexanderT29 <alextesoro96@gmail.com>
2026-06-25 15:15:52 +0200

    aggiunto Mixins per le chiamate rest

--- peoplehub/api/urls.py
+++ peoplehub/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('singleobj/<int:id>/', views.singleobj),
-    path('multipleobj', views.multipleobj)
+    path('singleobj/<int:id>/', views.singleObjAPIView.as_view()),
+    path('multipleobj', views.multipleObjAPIView.as_view())
 ]
--- peoplehub/api/views.py
+++ peoplehub/api/views.py
@@ -9,41 +9,78 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, DestroyModelMixin
 
-@csrf_exempt
+@api_view(['GET','PUT','PATCH'])
 def singleobj(request, id):
     data = get_object_or_404(Person, id=id)
     if request.method == 'PUT':
-        stream = io.BytesIO(request.body)
-        parsed_data = JSONParser().parse(stream)
+        parsed_data = request.data
         serializer = PersonModelSerializer(data, data=parsed_data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return JsonResponse({'update':'success'})
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'update':'success'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'PATCH':
-        stream = io.BytesIO(request.body)
-        parsed_data = JSONParser().parse(stream)
+        parsed_data = request.data
         serializer = PersonModelSerializer(data, data=parsed_data, partial=True)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return JsonResponse({'update':'success'})
-    serializer = PersonModelSerializer(data)
-    return JsonResponse(serializer.data)
+            return Response({'update':'success'})
+    if request.method == 'GET':
+        serializer = PersonModelSerializer(data)
+        return Response(serializer.data)
+    
     
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def multipleobj(request):
     if request.method == "POST":
-        json = request.body
-        stream = io.BytesIO(json)
-        parsed_data = JSONParser().parse(stream)
+        parsed_data = request.data
         serializer = PersonModelSerializer(data=parsed_data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return JsonResponse({"created":"successfull"}, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"created":"successfull"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        print(request.accepted_renderer)
+        data = Person.objects.all()
+        serializer = PersonModelSerializer(data, many=True)
+        return Response(serializer.data)
+    
+class multipleObjAPIView(CreateModelMixin, ListModelMixin, GenericAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonModelSerializer
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    data = Person.objects.all()
-    serializer = PersonModelSerializer(data, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    def post(self, request):
+        return self.create(request)
+    
+class singleObjAPIView(GenericAPIView):
+
+    def get(self, request, id):
+        data = get_object_or_404(Person, id=id)
+        serializer = PersonModelSerializer(data)
+        return Response(serializer.data)
+
+    def put(self,request, id):
+        data = get_object_or_404(Person, id=id)
+        parsed_data = request.data
+        serializer = PersonModelSerializer(data, data=parsed_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'update':'success'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id):
+        data = get_object_or_404(Person, id=id)
+        parsed_data = request.data
+        serializer = PersonModelSerializer(data, data=parsed_data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'update':'success'})
